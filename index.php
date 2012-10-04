53aa278298ee4d56be77200e790042bfd5c51aff
mahlad <mahlad365@gmail.com>
2012-10-04 11:15:04 +0330

    add index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8"/>
+	<title>بهار اندیشان معماری شرق</title>
+	<link type="text/css"rel="stylesheet" href="css/reset.css"/>
+	<link type="text/css"rel="stylesheet" href="css/1style.css"/>
+	<link type="text/css"rel="stylesheet" href="css/style.css"/>
+	<script type="text/javascript" src="script/html5shiv.js"></script>
+	<script type="text/javascript" src="script/zepto.js"></script>
+	<script type="text/javascript" src="script/arcbam.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
