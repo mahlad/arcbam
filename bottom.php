72e0d3e8edeff805a2b06302c6c79f7a23ab4846
mahlad <mahlad365@gmail.com>
2012-10-04 19:58:52 +0330

    navigation main menu

--- bottom.php
+++ bottom.php
@@ -2,5 +2,14 @@
 	<div class="logo">
 		<img src="images/logo.png" alt="">
 	</div>
+	<nav>
+		<ul>
+			<li class="home"><a href="" title="">صفحه اصلی</a> </li>
+			<li class="project"><a href="" title="">پروژه ها</a> </li>
+			<li class="marketting"><a href="" title="">تبلیغات</a> </li>
+			<li class="learn"><a href="" title="">آموزش</a> </li>
+			<li class="contact"><a href="" title="">تماس با ما</a> </li>
+		</ul>
+	</nav>
 
 </div>
