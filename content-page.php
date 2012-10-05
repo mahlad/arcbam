a0ff0fab62b6a90663b38db972eb469468c1f97f
mahlad <mahlad365@gmail.com>
2012-10-06 00:04:49 +0330

    images for fadeloop slideshow

--- content-page.php
+++ content-page.php
@@ -1,3 +1,8 @@
 <div class="content">
-
+	<img src="images/fadeloop/design-1.jpg" alt="">
+	<img src="images/fadeloop/design-2.jpg" alt="">
+	<img src="images/fadeloop/design-3.jpg" alt="">
+	<img src="images/fadeloop/design-4.jpg" alt="">
+	<img src="images/fadeloop/design-5.jpg" alt="">
+	<img src="images/fadeloop/design-6.jpg" alt="">
 </div>
