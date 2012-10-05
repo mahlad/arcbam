7aae9bc5ddc2b4e05ef6161c527edff41d2f2e32
mahlad <mahlad365@gmail.com>
2012-10-05 23:46:01 +0330

    add customised cods slideshow tile

---
+++ script/arcbam.js
@@ -0,0 +1,34 @@
+storyBoard = {
+	puzzel1 : {
+		delay : 0,
+		target : '.pone',
+		keyframe : 'puzzel1',
+		due : 1000,
+		ease : 'ease-out'
+	},
+	puzzel2 : {
+		delay : 500,
+		target : '.ptow',
+		keyframe : 'puzzel2',
+		due : 1000,
+		ease : 'ease-out'
+	},
+	puzzel3 : {
+		delay : 1000,
+		target : '.pthree',
+		keyframe : 'puzzel3',
+		due : 1000,
+		ease : 'ease-out'
+	},
+	puzzel4 : {
+		delay : 1500,
+		target : '.pfour',
+		keyframe : 'puzzel4',
+		due : 1000,
+		ease : 'ease-out'
+	}
+};
+
+$(function(){
+	$.tween(storyBoard);
+})
