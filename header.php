8d924ac89635beaf35cfd6c302fbc4af3a28ccd5
mahlad <mahlad365@gmail.com>
2012-10-07 20:48:46 +0330

    prepare site and fix it in IE

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
 	<title>بهار اندیشان معماری شرق</title>
 	<link type="text/css"rel="stylesheet" href="css/reset.css"/>
 	<link type="text/css"rel="stylesheet" href="css/1styles.css"/>
