83f0b7ed923bf43bd92a1d937a14379fa4199107
mahlad <mahlad365@gmail.com>
2012-10-08 00:21:51 +0330

    insert images in dynomic state

--- projects.php
+++ projects.php
@@ -6,7 +6,19 @@
 <div class="content">
 	<div class="projects w13 right">
 		<section>
+			<div class="bigpic">
+				<?php 
+				$path='./images/projects';
+				$img_arr=scandir($path);
+				foreach ($img_arr as $img) {
+					$img_type=explode('.',$img);
+					$img_type=strtolower(end($img_type));
+					if($img_type=='jpg')
+					echo "<div style=\"background-image:url('$path/$img');\"></div>";
 
+				}
+				 ?>
+			</div>
 		</section>
 		<ul>
 			<li></li>
