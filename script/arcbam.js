d08d01677caba99a055f73dfad45bcfe0e1b3bba
mahlad <mahlad365@gmail.com>
2012-10-08 19:40:18 +0330

    set zepto for show the buttons when mouseover the ul in smartpart

--- script/arcbam.js
+++ script/arcbam.js
@@ -41,10 +41,14 @@ $(function(){
 /*-----------Bottom Content slideshow on projects---------*/
 $(function(){
 	var bcsp=$('div.projects > section'),
-		bcsp_pic=$('div.projects > section.bigpart div.bigpic'),
-		bcsp_txt=$('div.projects > section.bigpart div.bigtxt'),
-		bcsp_spic=$('div.projects > ul > li'),
-		bcsp_total=$('div.projects > section.bigpart div.bigpic > div').length,
+		bcsp_pic=$('div.projects section.bigpart div.bigpic'),
+		bcsp_txt=$('div.projects section.bigpart div.bigtxt'),
+		bcsp_total=$('div.projects section.bigpart div.bigpic > div').length,
+		bcsp_smallpart=$('div.projects section.smallpart'),
+		bcsp_spic=$('div.projects section.smallpart > ul > li'),
+		bcsp_btn=$('div.projects div.btn'),
+		bcsp_prev=$('div.projects div.prev'),
+		bcsp_next=$('div.projects div.next'),
 		curentSlide=0;
 		(bcsp_go2slide=function(n){
 			if(n>bcsp_total-1)n=0;
@@ -58,6 +62,12 @@ $(function(){
 		bcsp_spic.click(function(){
 			bcsp_go2slide($(this).index());
 		});
+		//for next and previw button
+		visible_btn=function(){
+			bcsp_btn.css({'opacity':1});
+		};
+		bcsp_smallpart.mouseover(visible_btn);
+		
 		nextslide=function(){
 			//alert(1);
 			bcsp_go2slide(curentSlide+1);
@@ -73,8 +83,8 @@ $(function(){
 			auto=false;
 		};
 
-		bcsp.mouseout(autoplay);
-		bcsp.mouseover(stopauto);
+		// bcsp.mouseout(autoplay);
+		// bcsp.mouseover(stopauto);
 		
 
 });
