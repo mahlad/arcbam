a65a9a0b4048255ecb724686369ecd26af01f0f3
mahlad <mahlad365@gmail.com>
2012-10-10 00:00:34 +0330

    set the html cods

--- marketting.php
+++ marketting.php
@@ -5,7 +5,23 @@
 ?>
 <div class="content">
 	<div class="pages">
-		
+		<ul class="firstrow">
+			<li></li>
+			<li></li>
+		</ul>
+		<ul class="secondrow">
+			<li></li>
+			<li></li>
+		</ul>
+		<ul class="thirdrow">
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+		<ul class="fourthrow">
+			<li></li>
+			<li></li>
+		</ul>
 
 	</div>
 
