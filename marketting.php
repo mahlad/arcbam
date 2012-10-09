f2ed4f4f3340e52f0781e79fd239aed847e53ae9
mahlad <mahlad365@gmail.com>
2012-10-10 00:32:42 +0330

    set the tow of ul position

--- marketting.php
+++ marketting.php
@@ -4,24 +4,24 @@
 	include 'bottom.php';
 ?>
 <div class="content">
-	<div class="pages">
-		<ul class="firstrow">
-			<li></li>
-			<li></li>
+	<div class="pages mrkt relative right">
+		<ul class="firstrow absolute">
+			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
 		</ul>
-		<ul class="secondrow">
-			<li></li>
-			<li></li>
+		<ul class="secondrow absolute">
+			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
 		</ul>
-		<ul class="thirdrow">
-			<li></li>
-			<li></li>
-			<li></li>
+		<!-- <ul class="thirdrow absolute">
+			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
 		</ul>
-		<ul class="fourthrow">
-			<li></li>
-			<li></li>
-		</ul>
+		<ul class="fourthrow absolute">
+			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
+		</ul> -->
 
 	</div>
 
