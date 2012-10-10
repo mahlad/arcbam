bce5697eeafe62064cf6c8ed01fb355bacbd2069
mahlad <mahlad365@gmail.com>
2012-10-10 16:07:44 +0330

    add picture of logo of each company

--- marketting.php
+++ marketting.php
@@ -6,21 +6,21 @@
 <div class="content">
 	<div class="marketting relative right">
 		<ul class="firstrow absolute">
-			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
-			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="images/marketting/small/company-1.jpg" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="images/marketting/small/company-2.jpg" alt=""></a></li>
 		</ul>
 		<ul class="secondrow absolute">
-			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
-			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="images/marketting/small/company-3.jpg" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="images/marketting/small/company-4.gif" alt=""></a></li>
 		</ul>
 		<ul class="thirdrow absolute">
-			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
-			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
-			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="images/marketting/small/company-5.png" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="images/marketting/small/company-6.gif" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="images/marketting/small/company-7.jpg" alt=""></a></li>
 		</ul>
 		<ul class="fourthrow absolute">
-			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
-			<li class="right"><a href="" title=""><img src="" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="images/marketting/small/company-8.gif" alt=""></a></li>
+			<li class="right"><a href="" title=""><img src="images/marketting/small/company-9.gif" alt=""></a></li>
 		</ul>
 
 	</div>
