a23a3d351c51bd8f67e05b26a7b6f2bd86b878ab
mahlad <mahlad365@gmail.com>
2012-10-10 18:03:53 +0330

    add big picture when click

--- marketting.php
+++ marketting.php
@@ -6,23 +6,26 @@
 <div class="content">
 	<div class="marketting relative right">
 		<ul class="firstrow absolute">
-			<li class="right"><a href="" title=""><img src="images/marketting/small/company-1.jpg" alt=""></a></li>
-			<li class="right"><a href="" title=""><img src="images/marketting/small/company-2.jpg" alt=""></a></li>
+			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
+			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
 		</ul>
 		<ul class="secondrow absolute">
-			<li class="right"><a href="" title=""><img src="images/marketting/small/company-3.jpg" alt=""></a></li>
-			<li class="right"><a href="" title=""><img src="images/marketting/small/company-4.gif" alt=""></a></li>
+			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
+			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
 		</ul>
 		<ul class="thirdrow absolute">
-			<li class="right"><a href="" title=""><img src="images/marketting/small/company-5.png" alt=""></a></li>
-			<li class="right"><a href="" title=""><img src="images/marketting/small/company-6.gif" alt=""></a></li>
-			<li class="right"><a href="" title=""><img src="images/marketting/small/company-7.jpg" alt=""></a></li>
+			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
+			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
+			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
 		</ul>
 		<ul class="fourthrow absolute">
-			<li class="right"><a href="" title=""><img src="images/marketting/small/company-8.gif" alt=""></a></li>
-			<li class="right"><a href="" title=""><img src="images/marketting/small/company-9.gif" alt=""></a></li>
+			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
+			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
 		</ul>
 
 	</div>
 
 </div>
+<?php 
+	include 'footer.php';
+ ?>
