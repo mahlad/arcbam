f764f12ef7ba5b50e9c9e0820b3903538f2d423f
mahlad <mahlad365@gmail.com>
2012-10-12 12:08:42 +0330

    set the table with tr and td that I need

--- contact.php
+++ contact.php
@@ -3,7 +3,60 @@
 	include 'sidebar.php';
 	include 'bottom.php';
 ?>
-
+<div class="content">
+<table class="contact">
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+</table>
+</div>
 <?php 
 	include 'footer.php';
  ?>
