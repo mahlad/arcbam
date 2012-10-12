eb3b2ebeacc55504b9014a83c3e23779cb8403b1
mahlad <mahlad365@gmail.com>
2012-10-12 13:05:26 +0330

    add class for settings

--- contact.php
+++ contact.php
@@ -6,26 +6,26 @@
 <div class="content">
 <table class="contact">
 	<tr>
-		<td  rowspan="5"class="picowner">
+		<td  rowspan="5" class="picowner">
 			<img src="images/picofowner.jpg">
 		</td>
 		
 	</tr>
 	<tr>
-		<td><p>شماره ثابت:</p></td>
-		<td><p>05116093979</p></td>
+		<td class="owninfo1"><p>شماره ثابت:</p></td>
+		<td class="owninfo2"><p>05116093979</p></td>
 	</tr>
 	<tr>
-		<td><p>شماره همراه:</p></td>
-		<td><p>09158203969</p></td>
+		<td class="owninfo1"><p>شماره همراه:</p></td>
+		<td class="owninfo2"><p>09158203969</p></td>
 	</tr>
 	<tr>
-		<td><p>ایمیل:</p></td>
-		<td><p>ar_pahlavan@yahoo.com</p></td>
+		<td class="owninfo1"><p>ایمیل:</p></td>
+		<td class="owninfo2"><p>ar_pahlavan@yahoo.com</p></td>
 	</tr>
 	<tr>
-		<td><p>آدرس:</p></td>
-		<td><p>مشهد ، خیابان سید رضی، بین سید رضی 42 و 44 ، پلاک 140، طبقه اول</p></td>
+		<td class="owninfo1"><p>آدرس:</p></td>
+		<td class="owninfo2"><p>مشهد ، خیابان سید رضی، بین سید رضی 42 و 44 ، پلاک 140، طبقه اول</p></td>
 	</tr>
 	<tr>
 		<td colspan="3" class="peygham"><p>کاربر محترم درصورت تمایل به ارسال پیام و یا سفارش کار، لطفا فرم زیر را تکمیل و ارسال نمایید</p></td>
