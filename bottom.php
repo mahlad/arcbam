f618ce8ec68188269634dda2903c02b82989c3d0
mahlad <mahlad365@gmail.com>
2012-10-13 09:09:53 +0330

    add link of new page

--- bottom.php
+++ bottom.php
@@ -7,7 +7,7 @@
 			<li class="home left"><a href="./" title="">صفحه اصلی</a> </li>
 			<li class="project left"><a href="projects.php" title="">پروژه ها</a> </li>
 			<li class="marketting left"><a href="marketting.php" title="">تبلیغات</a> </li>
-			<li class="learn left"><a href="" title="">آموزش</a> </li>
+			<li class="learn left"><a href="learning.php" title="">آموزش</a> </li>
 			<li class="contact left"><a href="contact.php" title="">تماس با ما</a> </li>
 		</ul>
 	</nav>
