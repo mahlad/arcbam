bfd2c8b556c38a04dcf4f96ebbe4cf3e893eb5eb
mahlad <mahlad365@gmail.com>
2012-10-13 10:35:51 +0330

    for set the border in right and bottom I need some changes

--- learning.php
+++ learning.php
@@ -20,9 +20,12 @@
 			</div>
 			<p class="context right overhidden txj">
 				امروزه تغييرات وتحولات مربوط به علم وپيشرفتهاي شگرف بشر در زمينه هاي مختلف علمي  ازچند دهه به هفته وياچند دقيقه كاهش پيدانموده است ودراين زمان اهميت وضرورت ارتقاء استاندارها ومديرت بهينه امكانات ونيروها در راستاي توسعه وبهبود هرچه بيشتر دستاوردهاي مختلف درپروژه هاي گوناگون بسيار نمايان تر شده است.
+
 			</p>
 			<a href="#" class="download left">دریافت متن کامل</a>
+			<hr class="left">
 		</section>
+
 		<section class="part right">
 			<div class="pic right">
 				<img src="images/learning/maghale.jpg" alt="">
