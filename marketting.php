03d2902e8a35f4c0acf8c03720bc86d91d5bdefb
mahlad <mahlad365@gmail.com>
2012-10-15 18:26:00 +0330

    for correct the mistake in hover on each part of company for rotate

--- marketting.php
+++ marketting.php
@@ -5,24 +5,77 @@
 ?>
 <div class="content">
 	<div class="marketting relative right">
-		<ul class="firstrow absolute">
-			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
-			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
-		</ul>
-		<ul class="secondrow absolute">
-			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
-			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
-		</ul>
-		<ul class="thirdrow absolute">
-			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
-			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
-			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
-		</ul>
-		<ul class="fourthrow absolute">
-			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
-			<li class="right"><a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a></li>
-		</ul>
-
+		<div class="firstrow absolute">
+			<div class="parentli right">
+				<div class="pic">
+					<img src="images/marketting/small/logos-icon.jpg" alt="">
+				</div>
+				<div class="text"><a href="images/marketting/mrkt.jpg" title="">برای آشنایی بیشتر با شرکت ...کلیک کنید</a></div>
+			</div>
+			<div class="parentli right">
+				<div class="pic">
+					<a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a>
+				</div>
+				<div class="text"><a href="images/marketting/mrkt.jpg" title="">برای آشنایی بیشتر با شرکت ...کلیک کنید</a></div>
+			</div>
+			<div class="cover"></div>
+		</div>
+		<div class="cover"></div>
+		<div class="secondrow absolute">
+			<div class="parentli right">
+				<div class="pic">
+					<a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a>
+				</div>
+				<div class="text"><a href="images/marketting/mrkt.jpg" title="">برای آشنایی بیشتر با شرکت ...کلیک کنید</a></div>
+			</div>
+			<div class="parentli right">
+				<div class="pic">
+					<a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a>
+				</div>
+				<div class="text"><a href="images/marketting/mrkt.jpg" title="">برای آشنایی بیشتر با شرکت ...کلیک کنید</a></div>
+			</div>
+			<div class="cover"></div>
+		</div>
+		<div class="cover"></div>
+		<div class="thirdrow absolute">
+			<div class="parentli right">
+				<div class="pic">
+					<a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a>
+				</div>
+				<div class="text"><a href="images/marketting/mrkt.jpg" title="">برای آشنایی بیشتر با شرکت ...کلیک کنید</a></div>
+			</div>
+			<div class="parentli right">
+				<div class="pic">
+					<a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a>
+				</div>
+				<div class="text"><a href="images/marketting/mrkt.jpg" title="">برای آشنایی بیشتر با شرکت ...کلیک کنید</a></div>
+			</div>
+			<div class="parentli right">
+				<div class="pic">
+					<a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a>
+				</div>
+				<div class="text"><a href="images/marketting/mrkt.jpg" title="">برای آشنایی بیشتر با شرکت ...کلیک کنید</a></div>
+			</div>
+			<div class="cover"></div>
+		</div>
+		<div class="cover"></div>
+		<div class="fourthrow absolute">
+			<div class="parentli right">
+				<div class="pic">
+					<a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a>
+				</div>
+				<div class="text"><a href="images/marketting/mrkt.jpg" title="">برای آشنایی بیشتر با شرکت ...کلیک کنید</a></div>
+			</div>
+			<div class="parentli right">
+				<div class="pic">
+					<a href="images/marketting/mrkt.jpg" title=""><img src="images/marketting/small/logos-icon.jpg" alt=""></a>
+				</div>
+				<div class="text"><a href="images/marketting/mrkt.jpg" title="">برای آشنایی بیشتر با شرکت ...کلیک کنید</a></div>
+			</div>
+			<div class="cover"></div>
+		</div>
+		<div class="cover"></div>
+		
 	</div>
 
 </div>
